acb2cae281bb8d30ec0a31b1dd75ef07a78279fb
Даниил Мосолов <dkuets@gmail.com>
2024-03-10 14:08:24 +0300

    create popups.js

---
+++ src/scripts/components/popups.js
@@ -0,0 +1,31 @@
+function openPopup(popup) {
+    popup.classList.add('popup_is-opened')
+    document.addEventListener('keydown', (e) => keyHandler(e, popup))
+}
+
+function closePopup(popup) {
+    popup.classList.remove('popup_is-opened')
+    document.removeEventListener('keydown', (e) => keyHandler(e, popup))
+}
+
+function keyHandler(e, popup) {
+    if (e.key === 'Escape') {
+        closePopup(popup)
+    }
+}
+
+function createPopup(popup, openBtn) {
+    const closeBtn = popup.querySelector('.popup__close')
+
+    // Добавление прослушивания событий
+    openBtn.addEventListener('click', () => openPopup(popup))
+    closeBtn.addEventListener('click', () => closePopup(popup))
+    // Обработка клика по Overlay
+    popup.addEventListener('click', (e) => {
+        if (e.currentTarget === e.target) {
+            closePopup(popup)
+        }
+    })
+}
+
+export {createPopup}
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,18 @@
 import '../pages/index.css'
 import {renderCards} from "./components/cards";
+import {createPopup} from "./components/popups";
 
 // Получение необходимых элементов и разметки
 const cardsTemplate = document.querySelector("#card-template").content; //Разметка карточки
 const cardsContainer = document.querySelector(".places__list");
+const editPopup = document.querySelector(".popup_type_edit");
+const newCardPopup = document.querySelector(".popup_type_new-card");
+const imagePopup = document.querySelector(".popup_type_image");
 
+const editPopupBtn = document.querySelector(".profile__edit-button");
+const newCardPopupBtn = document.querySelector(".profile__add-button");
+
+createPopup(editPopup, editPopupBtn)
+createPopup(newCardPopup, newCardPopupBtn)
 
 renderCards(cardsContainer, cardsTemplate)
